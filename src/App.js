0f959e92b7dd77666835308e22bddb3d368084ad
Rohit Dandnayak <rohit.dandnayak@ril.com>
2019-03-21 09:56:25 +0530

    authenticated routes

--- src/App.js
+++ src/App.js
@@ -36,7 +36,11 @@ class App extends Component {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link href="/explore"> Explore </Nav.Link>
+                {isAuthenticated ? (
+                  <Nav.Link href="/explore"> Explore </Nav.Link>
+                ) : (
+                  ''
+                )}
                 <Nav.Link href="#pricing" />
                 {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
